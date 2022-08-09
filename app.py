579e861fcf6dcef095e21d72fd0b47e33cea5a92
barema.aime <baremaaime@gmail.com>
2022-08-09 16:50:55 +0200

    add to codes to app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ from dash.dependencies import Input, Output
 
 # ----------------------------------------------------------------------------------------------------------------------
 
-app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = JupyterDash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 app.layout = html.Div(
     [
@@ -34,9 +34,33 @@ app.layout = html.Div(
         ),
         html.Br(),
         
-        html.Div(
-            [
-
+        dbc.Row(
+            children=[
+                dbc.Col(
+                    children=[
+                        html.H5("File Location"),
+                        dcc.Input(id="input-file-location", 
+                                  placeholder="directory/file.csv", 
+                                  type="text", value="water_cons_data.csv")
+                    ]
+                ),
+                dbc.Col(
+                    children=[
+                        html.H5("Time Range"),
+                        dcc.RadioItems(id="input-radio-item",
+                                       options=[dict(label=' 6 Months', value=6), 
+                                                dict(label=' 1 Year', value=12),
+                                                dict(label=' 2 Years', value=24)], 
+                                       value=12, labelStyle={'display': 'block'})
+                    ]
+                ),
+                dbc.Col(
+                    children=[
+                        html.H5("Sample Size"),
+                        dcc.Input(id="input-sample-size", placeholder='Enter a %', 
+                                  type='number', value=10)
+                    ]
+                )
             ]
         ),
         html.Br(),
@@ -53,9 +77,8 @@ app.layout = html.Div(
             children=[
                 dbc.Col(
                     children=[
-                        html.H5("K-clusters range"),
+                        html.H5("K-Clusters Range"),
                         dcc.Slider(min=2, max=12, step=1, value=2, id="k-slider"),
-                        html.Div(id="k-cluster-output-text"),
                         dcc.Graph(id="pca-scatter")    
                     ],
                     width=6
@@ -73,62 +96,64 @@ app.layout = html.Div(
                 )
             ]
         ),
+        html.Br(),
         
-        dcc.Graph(id="groups-line-plot")
+        html.Div(
+            [
+                html.H3("Clusters Characteristics")
+            ],
+            style = {"text-align": "center"}
+        ),
+        html.Br(),
+        
+        dbc.Row(
+            children=[
+                dbc.Col(dcc.Graph(id="groups-line-plot"), width=6),
+                dbc.Col(dcc.Graph(id="bar-plot"), width=6)
+            ]
+        )
     ]
 )
 
 
 # ----------------------------------------------------------------------------------------------------------------------
 
-def get_frame(filepath="water_cons_data.csv", sample_size=10, time_range=12):
-    df = pd.read_csv(filepath)
+def get_frame(fpath="water_cons_data.csv", n_size=10, t_range=12):
+    df = pd.read_csv(fpath)
     df.set_index("customer_identifier", inplace=True)
-    df_sample = df.sample(frac=sample_size * 0.01, random_state=42)
-    df = df_sample.iloc[:, -time_range:-1].copy()
+    df_sample = df.sample(frac=n_size * 0.01, random_state=42)
+    df = df_sample.iloc[:, -t_range:-1].copy()
     df["2021-Jun"] = df_sample["2021-Jun"].copy()
     
     return df
 
 # ----------------------------------------------------------------------------------------------------------------------
 
-def get_model(filepath="water_cons_data.csv", sample_size=10, time_range=12, k=2):
+def get_model(fpath="water_cons_data.csv", n_size=10, t_range=12, k=2):
     model = make_pipeline(
         StandardScaler(), KMeans(n_clusters=k, random_state=42)
     )
     model.fit(
-        get_frame(filepath=filepath, sample_size=sample_size, time_range=time_range)
+        get_frame(fpath=fpath, n_size=n_size, t_range=t_range)
     )
     
     return model
 
 # ----------------------------------------------------------------------------------------------------------------------
 
-def get_pca_labels(filepath="water_cons_data.csv", sample_size=10, time_range=12, k=2):
+def get_pca_labels(fpath="water_cons_data.csv", n_size=10, t_range=12, k=2):
     transformer = PCA(n_components=2, random_state=42)
     X_t = transformer.fit_transform(
-        get_frame(filepath=filepath, sample_size=sample_size, time_range=time_range)
+        get_frame(fpath=fpath, n_size=n_size, t_range=t_range)
     )
     X_pca = pd.DataFrame(X_t, columns=["PCA1", "PCA2"])
-    model = get_model(filepath=filepath, sample_size=sample_size, time_range=time_range, k=k)
+    model = get_model(fpath=fpath, n_size=n_size, t_range=t_range, k=k)
     X_pca["label"] = model.named_steps["kmeans"].labels_.astype(str)
     
     X_pca.sort_values("label", inplace=True)
     
     return X_pca
 
-# ----------------------------------------------------------------------------------------------------------------------
-
-@app.callback(
-    Output("k-cluster-output-text", "children"),
-    Input("k-slider", "value")
-)
-def serve_k_selected(k=2):
-    text = [
-        html.H6(f"Number of clusters (K): {k}")
-    ]
-    
-    return text
 
 # ----------------------------------------------------------------------------------------------------------------------
 
@@ -140,9 +165,9 @@ def serve_k_selected(k=2):
     Input("k-slider", "value")
 )
 
-def serve_scatter(filepath="water_cons_data.csv", sample_size=10, time_range=12, k=2):
+def serve_scatter(fpath="water_cons_data.csv", n_size=10, t_range=12, k=2):
     fig = px.scatter(
-        data_frame=get_pca_labels(filepath=filepath, sample_size=sample_size, time_range=time_range, k=k),
+        data_frame=get_pca_labels(fpath=fpath, n_size=n_size, t_range=t_range, k=k),
         x="PCA1", y="PCA2", color="label",
         title="PCA Representation of Clusters"
     )
@@ -153,6 +178,7 @@ def serve_scatter(filepath="water_cons_data.csv", sample_size=10, time_range=12,
 
 # ----------------------------------------------------------------------------------------------------------------------
 
+
 @app.callback(
     Output("scores-line-plot", "figure"),
     Input("input-file-location", "value"),
@@ -160,20 +186,19 @@ def serve_scatter(filepath="water_cons_data.csv", sample_size=10, time_range=12,
     Input("input-sample-size", "value"),
     Input("metrics-dropdown", "value")
 )
-def serve_metrics_plot(filepath="water_cons_data.csv", sample_size=10, time_range=12, iner_met="True"):
+def serve_metrics_plot(fpath="water_cons_data.csv", n_size=10, t_range=12, iner_met="True"):
     n_clusters = range(2, 13)
     inertia_errors = []
     silhouette_scores = []
 
     for k in n_clusters:
-        model = get_model(filepath="water_cons_data.csv", sample_size=10, 
-                          time_range=12, k=k)
-    inertia_errors.append(model.named_steps["kmeans"].inertia_)
-    silhouette_scores.append(
-        silhouette_score(
-            get_frame(filepath=filepath, sample_size=sample_size, time_range=time_range), 
-            model.named_steps["kmeans"].labels_)
-    )
+        model = get_model(fpath=fpath, n_size=n_size, t_range=t_range, k=k)
+        inertia_errors.append(model.named_steps["kmeans"].inertia_)
+        silhouette_scores.append(
+            silhouette_score(
+                get_frame(fpath=fpath, n_size=n_size, t_range=t_range), 
+                model.named_steps["kmeans"].labels_)
+        )
     
     if iner_met:
         metric = "Inertia"
@@ -191,6 +216,7 @@ def serve_metrics_plot(filepath="water_cons_data.csv", sample_size=10, time_rang
     
     return fig
 
+
 # ----------------------------------------------------------------------------------------------------------------------
 
 @app.callback(
@@ -200,9 +226,9 @@ def serve_metrics_plot(filepath="water_cons_data.csv", sample_size=10, time_rang
     Input("input-sample-size", "value"),
     Input("k-slider", "value")
 )
-def serve_consumption_plots(filepath="water_cons_data.csv", sample_size=10, time_range=12, k=2):
-    df = get_frame(filepath=filepath, sample_size=sample_size, time_range=time_range)
-    model = get_model(filepath=filepath, sample_size=sample_size, time_range=time_range, k=k)
+def serve_consumption_plots(fpath="water_cons_data.csv", n_size=10, t_range=12, k=2):
+    df = get_frame(fpath=fpath, n_size=n_size, t_range=t_range)
+    model = get_model(fpath=fpath, n_size=n_size, t_range=t_range, k=k)
     df["label"] = model.named_steps["kmeans"].labels_.astype(str)
     months = np.array(df.columns)
     
@@ -214,11 +240,39 @@ def serve_consumption_plots(filepath="water_cons_data.csv", sample_size=10, time
             x=months, y=np.array(cons), name=label, mode='lines'))
         
     fig.update_layout(
-        title="Consumption Across Multiple Groups",
+        title="Consumption Across Multiple Clusters",
         xaxis_title="Month", yaxis_title="Consumption"
     )
         
     return fig
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+@app.callback(
+    Output("bar-plot", "figure"),
+    Input("input-file-location", "value"),
+    Input("input-radio-item", "value"),
+    Input("input-sample-size", "value"),
+    Input("k-slider", "value")
+)
+def serve_bar_plot(fpath="water_cons_data.csv", n_size=10, t_range=12, k=2):
+    df = get_frame(fpath=fpath, n_size=n_size, t_range=t_range)
+    avg_df = df.mean(axis=1).to_frame()
+    avg_df.rename(columns={'0': 'avg'}, inplace=True)
+    model = get_model(fpath=fpath, n_size=n_size, t_range=t_range, k=k)
+    avg_df["label"] = model.named_steps["kmeans"].labels_.astype(str) 
+    gr_df = avg_df.groupby(["label"]).mean().sort_values(by=['mean'], ascending=False)
+    
+    fig = px.bar(
+        x=gr_df.index, y=gr_df['mean'],
+        title="Average Consumption Across Clusters"
+    )
+    
+    fig.update_layout(xaxis_title="Cluster", yaxis_title="Average")
+    
+    return fig
+
 
 # ----------------------------------------------------------------------------------------------------------------------
 
